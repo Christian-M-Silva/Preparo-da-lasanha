4a8c9ccc8dd108af7692a8bc8994f2df45558666
Christian-M-Silva <hoodprogrammer@gmail.com>
2022-03-03 06:35:34 -0300

    Layout do site concluido

---
+++ consult.js
@@ -0,0 +1,7 @@
+/*
+Preciso pegar o que a pessoa digitou no input como um argumento de preferência, tratar de acordo com o que o card pede, retorna o resultado final e assim que a pessoa clicar no botão esse valor vai para o alert junto com alguma mensagem.
+
+ Vai ser três functions diferentes, para cada function ele vai receber a mesma estrutura, parâmetros que receberam como argumento o valor que o usuário inseriu (ver como funciona o fluxo dos parâmetros no word que eu escrevi, se eu consegui usar variáveis, ai eu coloco nas variáveis o value do que a pessoa digitou), vai tratar esse argumento de acordo com o que o card pede, e no fim eu coloco um return resultado sendo que tem que ser nomes diferentes para cada card.
+
+ Criar mais 3 funções que terão um evento de click no botão, que dependendo do card vai retornar um alert, só que esse alerts terão que está dentro de um if e else pois dependendo da condição vai aparecer uma mensagem ou outra.
+*/
